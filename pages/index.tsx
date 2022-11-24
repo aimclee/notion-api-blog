8eacfbf82a35bf00bd9e88e2dc154301a607102d
aimclee <dhy04057@soongsil.ac.kr>
2022-11-24 17:45:15 +0900

    data 가져오기 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { GetStaticProps } from "next";
 import Image from "next/image";
+import { getDatabaseItems } from "../cms/notion";
 import PageHead from "../components/common/PageHead";
 import styles from "../styles/Home.module.css";
+import { CardData } from "../types/types";
+import { parseDatabaseItems } from "../utils/parseDatabaseItems";
 
-const Home: NextPage = () => {
+interface HomeProps {
+  data: CardData[];
+}
+
+const Home = ({ data }: HomeProps) => {
+  console.log("data :>> ", data);
+
   return (
     <>
       <PageHead />
@@ -13,23 +21,19 @@ const Home: NextPage = () => {
           <h1 className={styles.title}>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
           </h1>
-
           <p className={styles.description}>
             Get started by editing{" "}
             <code className={styles.code}>pages/index.tsx</code>
           </p>
-
           <div className={styles.grid}>
             <a href="https://nextjs.org/docs" className={styles.card}>
               <h2>Documentation &rarr;</h2>
               <p>Find in-depth information about Next.js features and API.</p>
             </a>
-
             <a href="https://nextjs.org/learn" className={styles.card}>
               <h2>Learn &rarr;</h2>
               <p>Learn about Next.js in an interactive course with quizzes!</p>
             </a>
-
             <a
               href="https://github.com/vercel/next.js/tree/canary/examples"
               className={styles.card}
@@ -37,7 +41,6 @@ const Home: NextPage = () => {
               <h2>Examples &rarr;</h2>
               <p>Discover and deploy boilerplate example Next.js projects.</p>
             </a>
-
             <a
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               className={styles.card}
@@ -49,7 +52,6 @@ const Home: NextPage = () => {
             </a>
           </div>
         </main>
-
         <footer className={styles.footer}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -73,3 +75,19 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const databaseId = process.env.DATABASE_ID;
+
+  if (!databaseId) throw new Error("DATABASE_ID is not defined");
+
+  const databaseItems = await getDatabaseItems(databaseId);
+
+  const parsedData = parseDatabaseItems(databaseItems);
+
+  return {
+    props: {
+      data: parsedData,
+    },
+  };
+};
