92b3c460c7f1396dd3e38dba0068c5c96a05065b
aimclee <dhy04057@soongsil.ac.kr>
2023-01-11 22:22:04 +0900

    footer changed

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import {
   AiFillGithub,
-  AiOutlineInstagram,
-  AiOutlineYoutube,
+  AiOutlineLinkedin,
 } from "react-icons/ai";
 
 const Footer = () => {
@@ -10,29 +9,22 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-cyan-500 to-blue-500 p-8 text-white">
       <div className="flex flex-row flex-wrap justify-around items-center max-w-5xl mx-auto">
         <a href="mailto:dev.caesiumy@gmail.com" className="hover:underline">
-          dev.caesiumy@gmail.com
+          dhy04057@gmail.com
         </a>
-        <p className="hover:underline">&copy;Caesiumy</p>
+        <p className="hover:underline">&copy;aimclee</p>
         <div>
           <ul className="flex flex-row flex-wrap justify-center items-center gap-2">
             <li className="hover:text-white/50">
-              <a href="https://github.com/" target="_blank" rel="noreferrer">
+              <a href="https://github.com/aimclee" target="_blank" rel="noreferrer">
                 <span>
                   <AiFillGithub size="2rem" />
                 </span>
               </a>
             </li>
             <li className="hover:text-white/50">
-              <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <a href="https://www.linkedin.com/in/andy-aimclee/" target="_blank" rel="noreferrer">
                 <span>
-                  <AiOutlineInstagram size="2rem" />
-                </span>
-              </a>
-            </li>
-            <li className="hover:text-white/50">
-              <a href="https://youtube.com" target="_blank" rel="noreferrer">
-                <span>
-                  <AiOutlineYoutube size="2rem" />
+                  <AiOutlineLinkedin size="2rem" />
                 </span>
               </a>
             </li>
